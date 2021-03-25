f9cc85989e3ebdc57e071855a60e6a99a9da7bae
Ancelmo Luiz <ancelmosantos@setic.ro.gov.br>
2021-03-24 22:01:50 -0400

    primeiro envio do projeto

---
+++ ApiEscola/Controllers/AlunoController.cs
@@ -0,0 +1,44 @@
+﻿using ApiEscola.Factories;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiEscola.Controllers
+{
+    [ApiController]
+    [Route("api/escola")]
+    public class AlunoController : Controller
+    {
+        //https://localhost:5000/api/escola/alunos
+        [HttpGet("alunos")]
+        public IActionResult ListarTodosAluno()
+        {
+            var alunos = AlunoFactory.Listar();
+            if (alunos != null)
+            {
+                return Ok(alunos);
+            }
+
+            return BadRequest(new { msg = "Nenhum aluno na lista" });
+        }
+
+        //https://localhost:5000/api/escola/aluno/1
+        [HttpGet("aluno/{id}")]
+        public IActionResult BuscarAlunoPeloId(int id)
+        {
+            var alunos = AlunoFactory.Listar();
+            var aluno = AlunoFactory.Buscar(ref alunos, id);
+
+            if (aluno != null)
+            {
+                return Ok(aluno);
+            }
+
+            return BadRequest(new { msg = "Aluno não encontrado" });
+        }
+
+
+    }
+}
---
+++ ApiEscola/Factories/AlunoFactory.cs
@@ -0,0 +1,31 @@
+﻿using ApiEscola.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiEscola.Factories
+{
+    public static class AlunoFactory
+    {
+        public static IList<AlunoViewModel> Listar()
+        {
+            var alunos = new List<AlunoViewModel>();
+
+            alunos.Add(new AlunoViewModel { Id = 1, Nome = "Douglas", Matricula = "1010" });
+            alunos.Add(new AlunoViewModel { Id = 2, Nome = "Billy", Matricula = "1015" });
+            alunos.Add(new AlunoViewModel { Id = 3, Nome = "Felipe Zomer", Matricula = "1020" });
+            alunos.Add(new AlunoViewModel { Id = 4, Nome = "Taiz Perez", Matricula = "2020" });
+            alunos.Add(new AlunoViewModel { Id = 5, Nome = "Yann", Matricula = "5025" });
+
+            return alunos;
+        }
+
+        public static AlunoViewModel Buscar(ref IList<AlunoViewModel> alunos, int id)
+        {
+            var aluno = alunos.FirstOrDefault(x => x.Id == id);
+
+            return aluno;
+        }
+    }
+}
