41f65cbf27bc092fe6e61312fcf5374ca76978e7
Ancelmo Luiz <ancelmosantos@setic.ro.gov.br>
2021-03-24 22:04:56 -0400

    adicionei a viewmodel de curso

---
+++ ApiEscola/Models/CursoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiEscola.Models
+{
+    public class CursoViewModel
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+    }
+}
