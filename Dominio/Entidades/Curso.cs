1f1ca67ec5b38bad09db50559137af0227ab6af0
Ancelmo Luiz <ancelmosantos@setic.ro.gov.br>
2021-03-31 20:33:55 -0400

    tarefa de criar o domínio feita com sucesso

---
+++ Dominio/Entidades/Curso.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dominio.Entidades
+{
+    public class Curso
+    {
+        public Curso(string nome, string turno)
+        {
+            Nome = nome;
+            Turno = turno;
+        }
+
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public string Turno { get; private set; }
+
+        public IEnumerable<Aluno> Alunos { get; private set; }
+    }
+}
